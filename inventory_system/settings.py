33a44c73c0b622d6ae8af0fbd3daa7f700df2d27
dejetem <enyeoluwarotimisimon@gmail.com>
2025-01-11 23:48:16 +0100

    settings.py for debugging

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -17,6 +17,9 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 
+
+print("Environment variables:", os.environ)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
